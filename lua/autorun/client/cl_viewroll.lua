cf06c1f5fa7c79baf4f42f84c572dbf9e86bbaae
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-12-12 16:07:50 -0800

    rewritten to actually be based on sdk code

--- lua/autorun/client/cl_viewroll.lua
+++ lua/autorun/client/cl_viewroll.lua
@@ -2,10 +2,11 @@ local ENTITY = FindMetaTable("Entity")
 local PLAYER = FindMetaTable("Player")
 local aGetViewEntity = GetViewEntity
 local pInVehicle = PLAYER.InVehicle
-local eGetVelocity = ENTITY.GetVelocity
+local eGetAbsVelocity = ENTITY.GetAbsVelocity
 local eGetMoveType = ENTITY.GetMoveType
-local enabled = CreateClientConVar("cl_viewroll", 1, true, false, "Enables/disables view roll.", 0, 1)
-local intensity = CreateClientConVar("cl_viewroll_intensity", 1.0, true, false, "Controls view roll intensity.", 0.1, 10)
+local pGetWalkSpeed = PLAYER.GetWalkSpeed
+local enabled = CreateClientConVar("cl_roll", 1, true, false, "Enables/disables view roll.", 0, 1)
+local rAngle = CreateClientConVar("cl_rollangle", 0.5, true, false, "Controls view roll intensity.")
 
 hook.Add("CalcView", "ViewRoll", function(ply, origin, angles, fov, zNear, zFar)
     if !enabled:GetBool() or ply != aGetViewEntity() or pInVehicle(ply) then
@@ -19,7 +20,7 @@ hook.Add("CalcView", "ViewRoll", function(ply, origin, angles, fov, zNear, zFar)
     end
 
     -- REFERENCE: https://github.com/ValveSoftware/source-sdk-2013/blob/master/mp/src/game/shared/baseplayer_shared.cpp#L1728
-    local velocity = eGetVelocity(ply)
+    local velocity = eGetAbsVelocity(ply)
     local side = velocity:Dot(angles:Right())
     local sign = 1
 
@@ -27,7 +28,18 @@ hook.Add("CalcView", "ViewRoll", function(ply, origin, angles, fov, zNear, zFar)
         sign = -1
     end
 
-    side = math.Clamp(math.abs(side), 0, 1000) * intensity:GetFloat()
+    side = math.abs(side)
 
-    angles.r = angles.r + (sign * side) * 0.005
+    local value = rAngle:GetFloat()
+    local rollSpeed = pGetWalkSpeed(ply)
+
+    -- Hit 100% of rollangle at rollspeed. Below that get linear approx.
+    if side < rollSpeed then
+        side = side * value / rollSpeed
+    else
+        side = value
+    end
+
+    -- Scale by right/left sign
+    angles.r = angles.r + side * sign
 end)
